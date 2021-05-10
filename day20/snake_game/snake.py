3f149d77af98e9b4663826d27922c4e6b9b2d79b
mrcos <m2e5090@gmail.com>
2021-05-10 16:31:25 -0700

    extract snake

--- day20/snake_game/snake.py
+++ day20/snake_game/snake.py
@@ -1,22 +1,47 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+UP = 90
+RIGHT = 0
+DOWN = 270
+LEFT = 180
+
 
 class Snake:
-    snake = []
 
-    for x in range(0, 3):
-        x_pos = float(x * -20)
-        body = Turtle("square")
-        body.speed(2)
-        body.color("white")
-        body.penup()
-        body.goto(x=x_pos, y=0)
-        snake.append(body)
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for x in range(0, 3):
+            x_pos = float(x * -20)
+            body = Turtle("square")
+            body.color("white")
+            body.penup()
+            body.goto(x=x_pos, y=0)
+            self.snake.append(body)
 
     def move(self):
-        snake = self.snake
-        for segment_num in range(len(snake) - 1, 0, -1):
-            new_x = snake[segment_num - 1].xcor()
-            new_y = snake[segment_num - 1].ycor()
-            snake[segment_num].goto(new_x, new_y)
-        snake[0].forward(20)
+        for segment_num in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[segment_num - 1].xcor()
+            new_y = self.snake[segment_num - 1].ycor()
+            self.snake[segment_num].goto(new_x, new_y)
+        self.snake[0].forward(MOVE_DISTANCE)
+
+    def snake_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def snake_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def snake_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def snake_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
