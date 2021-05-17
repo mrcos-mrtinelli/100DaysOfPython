a2701aa15869d266bba5d572f10c431bc7d25f67
mrcos <m2e5090@gmail.com>
2021-05-17 10:23:31 -0700

    create data frame with pandas

--- day25/reading-csv/main.py
+++ day25/reading-csv/main.py
@@ -5,3 +5,14 @@ WEATHER_CSV = "weather_data.csv"
 data = pandas.read_csv(WEATHER_CSV)
 
 print((data[data.day == "Monday"].temp * 9/5) + 32)
+
+# creating data frame with pandas
+
+data_dict = {
+    "students": ["Amy", "Brittany", "Cameron"],
+    "grades": [99, 100, 69]
+}
+
+data_frame = pandas.DataFrame(data_dict)
+data_frame.to_csv("data_frame.csv")
+print(f"Data Frame:\n {data_frame}")
