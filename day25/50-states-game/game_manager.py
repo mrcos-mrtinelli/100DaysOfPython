b52e77361eddb55d88219349688fa25f1b4ac75c
mrcos <m2e5090@gmail.com>
2021-05-18 16:53:00 -0700

    add pandas solution

--- day25/50-states-game/game_manager.py
+++ day25/50-states-game/game_manager.py
@@ -1,35 +1,35 @@
 import turtle
+import pandas
 
 TOTAL_STATES = 50
 
 
-class Game_Manager:
+class GameManager:
 
-    def __init__(self):
-        self.all_states = []
+    def __init__(self, file):
+        self.df = pandas.read_csv(file)
+        self.state_list = self.df.state.tolist()
         self.score = 0
 
     def get_user_input(self):
         title = f"{self.score} / {TOTAL_STATES} States Guessed"
         prompt = "Enter the name of a state"
         state_name = turtle.textinput(title, prompt)
-        return state_name
+        return state_name.lower()
 
-    def is_input_correct(self, state_named):
-        for state in self.all_states:
-            if state_named.lower() in state.lower():
+    def is_answer_correct(self, state_named):
+        for state in self.state_list:
+            if state_named == state.lower():
                 self.score += 1
-                state_name = state.split(",")[0]
-                x_pos = state.split(",")[1]
-                y_pos = state.split(",")[2].replace("\n", "")
-                return state_name, x_pos, y_pos
+                return True
+        else:
+            return False
 
-    def add_state_label(self, state, x, y):
-        x_pos = int(x)
-        y_pos = int(y)
+    def add_state_label(self, state):
+        state_data = self.df[self.df.state.str.lower() == state]
         writer = turtle.Turtle()
         writer.hideturtle()
         writer.penup()
-        writer.setposition(x_pos, y_pos)
-        writer.write(state, False, "center", ("Arial", 16, "normal"))
+        writer.setposition(int(state_data.x), int(state_data.y))
+        writer.write(state_data.state.item(), False, "center", ("Courier", 12, "normal"))
 
