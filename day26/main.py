555f929e48bfb1539d6a22c8c52e204e85595210
mrcos <m2e5090@gmail.com>
2021-05-20 16:09:33 -0700

    add solution

--- day26/main.py
+++ day26/main.py
@@ -1,1 +1,17 @@
-import pandas
+import pandas as pd
+
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+
+df = pd.DataFrame(data)
+
+nato_dict = {row.letter: row.code for (i, row) in df.iterrows()}
+
+is_on = True
+while is_on:
+    name = input("Enter your name: ").upper()
+
+    if name == "exit()":
+        is_on = False
+    else:
+        phonetic_spelling = [nato_dict[letter] for letter in name]
+        print(phonetic_spelling)
