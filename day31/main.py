4aa835bdf44f4b19f51549f5b7d8937a3150b18d
mrcos <m2e5090@gmail.com>
2021-06-18 16:13:26 -0700

    add progress tracking

--- day31/main.py
+++ day31/main.py
@@ -6,42 +6,62 @@ BG_COLOR = '#B1DDC6'
 FONT_BG = '#FFFFFF'
 FONT = 'Arial'
 
-front_card_word = []
-back_card_word = []
+data = []
+word_to_learn = {}
+
 
 # ===============
 #   READ FILE
 # ===============
-filepath = 'data/french_words.csv'
-data = pandas.read_csv(filepath).to_dict(orient="records")
+def get_data():
+    global data
+    try:
+        filepath = 'data/words_to_learn.csv'
+        data = pandas.read_csv(filepath).to_dict(orient="records")
+    except FileNotFoundError:
+        filepath = 'data/french_words.csv'
+        data = pandas.read_csv(filepath).to_dict(orient="records")
+
+    if len(data) == 0:
+        print("All words learned!")
+        exit()
 
 
 # ===============
 #   MANAGE WORDS
 # ===============
 def get_new_word():
-    global front_card_word, back_card_word
-    word_pair = random.choice(data)
-    front_card_word = ['French', word_pair['French']]
-    back_card_word = ['English', word_pair['English']]
+    global word_to_learn
+    word_to_learn = random.choice(data)
+
+
+def save_progress(known_word):
+    data.remove(known_word)
+    df = pandas.DataFrame.from_dict(data)
+    df.to_csv('data/words_to_learn.csv', index=False, header=True)
+    print("saved@")
 
 
 # ===============
 #   MANAGE CARDS
 # ===============
-def next_card():
+def next_card(is_correct=False):
+    if is_correct:
+        save_progress(word_to_learn)
+
+    global flip_timer
+    w.after_cancel(flip_timer)
     canvas.itemconfig(current_card_image, image=card_front_png)
     get_new_word()
-    canvas.itemconfig(card_title, text=front_card_word[0])
-    canvas.itemconfig(card_word, text=front_card_word[1])
-
-    w.after(3000, func=flip_card)
+    canvas.itemconfig(card_title, text="French", fill="#000000")
+    canvas.itemconfig(card_word, text=word_to_learn['French'], fill="#000000")
+    flip_timer = w.after(3000, flip_card)
 
 
 def flip_card():
     canvas.itemconfig(current_card_image, image=card_back_png)
-    canvas.itemconfig(card_title, text=back_card_word[0])
-    canvas.itemconfig(card_word, text=back_card_word[1])
+    canvas.itemconfig(card_title, text="English", fill="#FFFFFF")
+    canvas.itemconfig(card_word, text=word_to_learn['English'], fill="#FFFFFF")
 
 
 # ===============
@@ -50,6 +70,7 @@ def flip_card():
 w = Tk()
 w.config(padx=50, pady=50, bg=BG_COLOR)
 w.title("French to English Flashcards")
+flip_timer = w.after(3000, next_card)
 
 # images
 card_front_png = PhotoImage(file='images/card_front.png')
@@ -68,7 +89,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 wrong_btn = Button(image=wrong_png, highlightthickness=0, bd=0, command=next_card)
 wrong_btn.grid(row=1, column=0)
 
-right_btn = Button(image=right_png, highlightthickness=0, bd=0, command=next_card)
+right_btn = Button(image=right_png, highlightthickness=0, bd=0, command=lambda: next_card(True))
 right_btn.grid(row=1, column=1)
 
 # labels
@@ -78,6 +99,7 @@ right_btn.grid(row=1, column=1)
 # word_label = Label(text="trouve", font=(FONT, 60, "bold"), width=22, justify="center", bg=FONT_BG)
 # word_label.place(x=15, y=263)
 
+get_data()
 next_card()
 
 w.mainloop()
