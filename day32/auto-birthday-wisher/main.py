cf3e048a0480f36e8bf96bdb8b57be97f215c7d1
mrcos <m2e5090@gmail.com>
2021-06-19 21:45:19 -0700

    add dataframe

--- day32/auto-birthday-wisher/main.py
+++ day32/auto-birthday-wisher/main.py
@@ -3,3 +3,17 @@ import pandas
 import random
 import smtplib
 
+BIRTHDAY_DF = pandas.read_csv('birthdays.csv')
+
+now = dt.datetime.now()
+current_month = now.month
+current_date = now.day
+
+val = BIRTHDAY_DF.values
+print(val[0])
+
+#
+# for person in list_of_dob['month']:
+#     if current_month == list_of_dob['month'][person]:
+#         for day in list_of_dob['day']:
+#             if current_date = list_of_dob['month'][month]:
