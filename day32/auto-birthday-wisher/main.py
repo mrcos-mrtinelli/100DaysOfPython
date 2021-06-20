9da19812fdd904a2f20074e5ef96248899b7b23b
mrcos <m2e5090@gmail.com>
2021-06-20 15:56:47 -0700

    add generate and send functions

--- day32/auto-birthday-wisher/main.py
+++ day32/auto-birthday-wisher/main.py
@@ -4,16 +4,42 @@ import random
 import smtplib
 
 BIRTHDAY_DF = pandas.read_csv('birthdays.csv')
+LETTER_PLACEHOLDER = "[NAME]"
+SENDER_EMAIL = ""
 
 now = dt.datetime.now()
 current_month = now.month
-current_date = now.day
+current_day = now.day
 
-val = BIRTHDAY_DF.values
-print(val[0])
 
-#
-# for person in list_of_dob['month']:
-#     if current_month == list_of_dob['month'][person]:
-#         for day in list_of_dob['day']:
-#             if current_date = list_of_dob['month'][month]:
+def check_birthdays():
+    for i, row in BIRTHDAY_DF.iterrows():
+        if row['month'] == current_month and row['day'] == current_day:
+            name = row['name']
+            email = row['email']
+            send_email(name, email)
+
+
+def generate_template(name):
+    number = random.randint(1, 3)
+    with open(f'letter_templates/letter_{number}.txt') as template:
+        template_str = template.read()
+        final_template = template_str.replace(LETTER_PLACEHOLDER, name)
+        return final_template
+
+
+def send_email(name, email):
+    message = generate_template(name)
+
+    if len(message) > 0 and len(name) > 0:
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user='', password='')
+            connection.sendmail(
+                from_addr=SENDER_EMAIL,
+                to_addrs=email,
+                msg=f"Subject: Happy Birthday!\n\n{message}"
+            )
+
+
+check_birthdays()
