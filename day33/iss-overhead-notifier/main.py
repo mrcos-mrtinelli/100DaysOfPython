b432a168b50917df38b76103ecd3fc414ba22d5d
mrcos <m2e5090@gmail.com>
2021-06-21 15:22:10 -0700

    add email functionality

--- day33/iss-overhead-notifier/main.py
+++ day33/iss-overhead-notifier/main.py
@@ -1,29 +1,68 @@
+import time
 from datetime import datetime
 import requests
-
-
+import smtplib
 
 # CONSTANTS
 ISS_API_URL = "http://api.open-notify.org/iss-now.json"
 SUNSET_RISE_API_URL = "https://api.sunrise-sunset.org/json"
-MY_LAT = 33.608768
-MY_LONG = -117.873360
-current_time = datetime.now().isoformat()
+MY_COORD = {
+    "lat": 33.608768,
+    "lng": -117.873360
+}
 
 
-# 33.598753361993815, -117.89499841466444
 def get_json(url, params=None):
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
 
 
+def is_visible(sunset, sunrise):
+    if sunset < current_time < sunrise:
+        return True
+    return False
+
+
+def is_in_range(my_coord, iss_coord):
+    my_lat1 = my_coord['lat'] - 5
+    my_lat2 = my_coord['lat'] + 5
+    my_lng1 = my_coord['lng'] - 5
+    my_lng2 = my_coord['lng'] + 5
+
+    if my_lat1 <= iss_coord['lat'] <= my_lat2:
+        if my_lng1 <= iss_coord['lng'] <= my_lng2:
+            return True
+    return False
+
+
+current_time = datetime.now().isoformat()
+
 iss_data = get_json(url=ISS_API_URL)
 iss_lat = iss_data['iss_position']['latitude']
 iss_long = iss_data['iss_position']['longitude']
+iss_coord = {
+    'lat': float(iss_lat),
+    'lng': float(iss_long)
+}
 
-sunset_rise_data = get_json(url=SUNSET_RISE_API_URL, params={"lat": MY_LAT, "lng": MY_LONG, "formatted": 0})
+sunset_rise_data = get_json(
+    url=SUNSET_RISE_API_URL,
+    params={
+        "lat": MY_COORD['lat'],
+        "lng": MY_COORD['lng'],
+        "formatted": 0
+    }
+)
 sunrise = sunset_rise_data['results']['sunrise']
 sunset = sunset_rise_data['results']['sunset']
+
+while True:
+    time.sleep(60)
+    if is_in_range(MY_COORD, iss_coord) and is_visible(sunset, sunrise):
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user='', password='')
+            connection.send_message("Subject:Look to the Sky!\n\nThe ISS is passing by, wave 👋🏼")
 
 
