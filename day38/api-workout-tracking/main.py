83dd1b8f684a5de8a0b8975350581123ed6c8a75
mrcos <m2e5090@gmail.com>
2021-06-23 16:14:00 -0700

    refactor

--- day38/api-workout-tracking/main.py
+++ day38/api-workout-tracking/main.py
@@ -1,17 +1,5 @@
-import requests
+from nutritionix import NutritionIX
 
-
-BASE_URL = "https://trackapi.nutritionix.com"
-HEADER = {
-    "x-app-id": APP_ID,
-    "x-app-key": APP_KEY
-}
-
-endpoint = "/v2/natural/exercise"
-full_url = f"{BASE_URL}{endpoint}"
-json_query = {"query":input("Enter your exercise: ")}
-
-response = requests.post(url=full_url,json=json_query, headers=HEADER)
-response.raise_for_status()
-print(response.text)
-
+user_query = input("What exercises did you do today? ")
+data_manager = NutritionIX(user_query)
+print(data_manager.exercise_data)
--- day38/api-workout-tracking/nutritionix.py
+++ day38/api-workout-tracking/nutritionix.py
@@ -1,5 +1,22 @@
+import credentials as cr
 import requests
 
+_BASE_URL = "https://trackapi.nutritionix.com"
+_HEADER = {
+    "x-app-id": cr.NUTRITIONIX['APP_ID'],
+    "x-app-key": cr.NUTRITIONIX['APP_KEY']
+}
 
 class NutritionIX:
-    pass
+
+    def __init__(self, query):
+        self.query = query
+        self.exercise_data = self.get_exercise_data()
+
+    def get_exercise_data(self):
+        endpoint = '/v2/natural/exercise'
+        json_query = {"query": self.query}
+        response = requests.post(url=f'{_BASE_URL}{endpoint}', json=json_query, headers=_HEADER)
+        response.raise_for_status()
+        return response.json()
+
