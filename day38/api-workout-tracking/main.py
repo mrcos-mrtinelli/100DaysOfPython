811585f8d1fa4167681bacb2a430b156a4d1fc62
mrcos <m2e5090@gmail.com>
2021-06-23 15:29:50 -0700

    install requests and test api

---
+++ day38/api-workout-tracking/main.py
@@ -0,0 +1,17 @@
+import requests
+
+APP_ID = ""
+APP_KEY = ""
+BASE_URL = "https://trackapi.nutritionix.com"
+HEADER = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY
+}
+
+endpoint = "/v2/natural/exercise"
+full_url = f"{BASE_URL}{endpoint}"
+json_query = {"query":input("Enter your exercise: ")}
+
+response = requests.post(url=full_url,json=json_query, headers=HEADER)
+response.raise_for_status()
+print(response.text)
