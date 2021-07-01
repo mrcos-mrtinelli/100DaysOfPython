02d6659c9649f4a6f37dbb385b49449ae29f8b8f
mrcos <m2e5090@gmail.com>
2021-06-30 17:35:45 -0700

    get and parse top 100

---
+++ day46/scraped-spotify-playlist/main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+
+BASE_URL = 'https://www.billboard.com/charts/hot-100/'
+is_correct_date = False
+
+# Get user input
+date_str = ""
+songs = []
+
+while not is_correct_date:
+    try:
+        date_str = input("Please enter a date in the format YYY-MM-DD: ")
+        date_time_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        is_correct_date = True
+        date_str = date_time_obj.strftime('%Y-%m-%d')
+    except ValueError:
+        print("Sorry, please enter the date in the correct format.\n")
+
+res = requests.get(url=f'{BASE_URL}{date_str}')
+soup = BeautifulSoup(res.text, 'html.parser')
+li_elements = soup.find_all(name="li", class_="chart-list__element")
+
+for el in li_elements:
+    songs.append(
+        {
+            "rank": el.find(class_="chart-element__rank__number").string,
+            "song": el.find(class_="chart-element__information__song").string,
+            "artist": el.find(class_="chart-element__information__artist").string
+        }
+    )
+
