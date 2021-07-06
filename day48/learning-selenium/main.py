1bfed477943b3cd6646b08fd0d58965326899a9c
mrcos <m2e5090@gmail.com>
2021-07-06 10:57:37 -0700

    add challenge solution

--- day48/learning-selenium/main.py
+++ day48/learning-selenium/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "/Users/marcos/chromedriver"
 
@@ -18,4 +19,16 @@ for idx, el in enumerate(events_elements):
 
 print(events_dict)
 
-driver.close()
+# driver.get("http://secure-retreat-92358.herokuapp.com/")
+# fname = driver.find_element_by_name("fName")
+# lname = driver.find_element_by_name("lName")
+# email = driver.find_element_by_name("email")
+# sign_up = driver.find_element_by_class_name("btn-primary")
+#
+# fname.send_keys("5w3469 rty8oefhsd")
+# lname.send_keys("74t8903rweuodfisl")
+# email.send_keys("edfosui@gerysh.com")
+# sign_up.click()
+
+
+# driver.close()
